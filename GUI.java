c9b1fe8b1675bc2ddeb21910c41e23786925ccdd
Alexander Obregon <alexander.obregon47@gmail.com>
2023-01-25 20:38:13 -0800

    Setup Jpanel, Jframe and basic import

---
+++ GUI.java
@@ -0,0 +1,25 @@
+import java.awt.GridLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class GUI {
+	
+	public GUI() {
+		JFrame frame = new JFrame();
+		
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+		panel.setLayout(new GridLayout());
+		
+	}
+
+	public static void main(String[] args) {
+		new GUI();
+		
+
+	}
+
+}
