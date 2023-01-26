db96becf7937807a5b17b3a47063c2f9cd2b1e93
Alexander Obregon <alexander.obregon47@gmail.com>
2023-01-25 20:57:40 -0800

    added counter to button press

--- GUI.java
+++ GUI.java
@@ -1,5 +1,7 @@
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
@@ -7,31 +9,48 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class GUI {
+public class GUI implements ActionListener {
+	
+	private int count = 0;
+	private JLabel label;
+	private JFrame frame;
+	private JPanel panel;
 	
 	public GUI() {
-		JFrame frame = new JFrame();
-		JButton button = new JButton("Clck me");
-		JLabel label = new JLabel("Number of clicks: 0");
+		frame = new JFrame();
 		
-		JPanel panel = new JPanel();
+		JButton button = new JButton("Clck me");
+		button.addActionListener(this);
+		
+		
+		label = new JLabel("Number of clicks: 0");
+		
+		
+		panel = new JPanel();
 		panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
-		panel.setLayout(new GridLayout());
-        panel.add(button);
-        panel.add(label);
-
-        frame.add(panel, BorderLayout.CENTER);
+		panel.setLayout(new GridLayout(0, 1));
+		panel.add(button);
+		panel.add(label);
+		
+		frame.add(panel, BorderLayout.CENTER);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setTitle("Our GUI");
 		frame.pack();
 		frame.setVisible(true);
-		
 	}
 
 	public static void main(String[] args) {
 		new GUI();
 		
 
+	}
+	
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		count++;
+		label.setText("Number of clicks: " + count);
+		
 	}
 
 }
